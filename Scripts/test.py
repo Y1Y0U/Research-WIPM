17d80978e364ca01ecad2ba8d39552c671eac27a
Y1Y0U <33246291+Y1Y0U@users.noreply.github.com>
2017-12-11 23:52:26 +0800

    Update test.py
    20171211 23.52 @home by XJ

--- Scripts/test.py
+++ Scripts/test.py
@@ -9,7 +9,7 @@ class Dataset1D(object):
 	FID plotting is possible
 	"""
 
-	def __init__(self, dataset_dir_path, exp_no, proc_no):
+	def __init__(self, dataset_dir_path, exp_no, proc_no,pos_shift=138):
 		'''
 		Requires the absolute directory path of 1D experiment, number of experiment. 
 		The number of processing is also needed, provided that the processed data is required
@@ -17,8 +17,8 @@ class Dataset1D(object):
 		self.ds_dir_path = dataset_dir_path
 		self.exp_no = exp_no
 		self.proc_no = proc_no
-		self.cmplx_fid = self.mkCplxFid()
-		self.cmplx_fid_len = len(self.cmplx_fid)
+		self.cmplx_fid = self.mkCplxFid(pos_shift)
+		self.proc_spec = self.getProcSpec()
 
 	def getFid(self):
 		try:
@@ -29,13 +29,13 @@ class Dataset1D(object):
 			print('FID file does not exist')
 		return temp
 
-	def mkCplxFid(self):
+	def mkCplxFid(self,pos_shift):
 		temp = self.getFid()
-		temp = np.concatenate((temp[138:], np.zeros(138)), axis=0)
+		temp = np.concatenate((temp[pos_shift:], np.zeros(pos_shift)), axis=0)
 		cmplx_fid = temp[0::2] + 1j * temp[1::2]
 		return cmplx_fid
 
-	def plotFid(self, part='r'):
+	def plotFid(self, part='r',pos_shift=138):
 		'''
 		plot either the real part ('r') or the imaginary part ('i') of the complex FID
 		'''
@@ -48,6 +48,20 @@ class Dataset1D(object):
 	def plotFT(self):
 		temp = np.fft.fft(self.cmplx_fid)
 		temp = np.fft.fftshift(temp)
+		plt.plot(temp)
+		plt.show()
+
+	def getProcSpec(self):
+		try:
+			proc_dir_path = self.ds_dir_path + str(self.exp_no) + '/pdata/' + str(self.proc_no)
+			os.chdir(proc_dir_path)
+			temp = np.fromfile('1r', dtype=np.int32)
+		except IOError:
+			print('Processed spectrum does not exist')
+		return temp		
+
+	def plotProcSpec(self):
+		temp = self.getProcSpec()
 		plt.plot(temp)
 		plt.show()
 
@@ -75,31 +89,37 @@ class RandSensing(object):
 	
 	"""
 	
-	def __init__(self, length_original_data):
-		self.len_original = length_original_data
-		self.GaussianRSM = self.mkGaussianRSM(len_measure=4096)
+	def __init__(self, original_dataset, sparsity=0.125):
+		self.len_original = len(original_dataset.proc_spec)
+		self.sparsity = sparsity
+		self.len_measure = np.floor(self.len_original*self.sparsity)
+		self.gaussianRSM = self.mkGaussianRSM()
+		self.g_measured_data = np.dot(self.gaussianRSM,original_dataset.proc_spec)
 	
-	def mkGaussianRSM(self, len_measure=4096):
-		temp = np.random.normal(0, 1.0/len_measure, len_measure*self.len_original)
-		temp = np.reshape(temp, (self.len_original, len_measure))
+	def mkGaussianRSM(self):
+		temp = np.random.normal(0, 1.0/self.len_measure, self.len_measure*self.len_original)
+		temp = np.reshape(temp, (-1, self.len_original))
 		return temp
-		
+
+	def mkBernoulliRSM(self):
+		temp = np.random.binomial(1,0.5, self.len_measure*self.len_original)
+		temp = np.reshape(temp, (-1, self.len_original))
+		return temp
 
 if __name__ == "__main__":
 
-	dataset_dir_path='C:/Bruker/TopSpin3.5.b.91pl7/examdata/NMR Data/xj-20171117/'
-	exp_no = 100  
+#	dataset_dir_path='C:/Bruker/TopSpin3.5.b.91pl7/examdata/NMR Data/xj-20171117/'
+	dataset_dir_path='C:/NMR Spectra/156574_0001csol/'
+	exp_no = 10  
 	proc_no = 1
-	test_dataset = Dataset1D(dataset_dir_path, exp_no, proc_no)
-	test_dataset.plotFT()
+	test_dataset = Dataset1D(dataset_dir_path, exp_no, proc_no,pos_shift=125)
+#	test_dataset.plotFid()
+#	test_dataset.plotProcSpec()
 	
-	print(test_dataset.cmplx_fid_len)
 	
-	test_RS = RandSensing(test_dataset.cmplx_fid_len)
+	test_RS = RandSensing(test_dataset)
+	
+#	plt.plot(test_RS.g_measured_data)
+#	plt.show()
 
-	plt.matshow(test_RS.GaussianRSM)
-	plt.show()
-	
-	
-	
-	
+	print(np.random.binomial(1, 0.5, len_measure*self.len_original))
